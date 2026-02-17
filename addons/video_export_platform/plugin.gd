6bdedabbd2db7c8c89685dff654a1e7d00bc83a1
NovaDC <164206531+NovaDC@users.noreply.github.com>
2026-02-16 19:41:02 -0600

    Assist script documentation loading
    Makes the scripts automatically load in the editor when appropriate, to make the editor documentation viewer more reliable for those scripts.

--- addons/video_export_platform/plugin.gd
+++ addons/video_export_platform/plugin.gd
@@ -1,35 +1,51 @@
 @tool
 extends EditorPlugin
 
-const PLUGIN_NAME := "video_export_platform"
+const PLUGIN_NAME := "Video Export Platform"
+
+const PLUGIN_NAME_INTERNAL := "video_export_platform"
+
+const ENSURE_SCRIPT_DOCS:Array[Script] = [
+    preload("./video_export_platform.gd")
+]
 
 var _export_platform_ref:VideoEditorExportPlatform = null
 
-func _get_plugin_name():
+# Every once ands a while the script docs simply refuse to update properly.
+# This nudges the docs into a ensuring that the important scripts added by
+# this addon are actually loaded.
+func _ensure_script_docs() -> void:
+	var edit := get_editor_interface().get_script_editor()
+	for scr in ENSURE_SCRIPT_DOCS:
+		edit.update_docs_from_script(scr)
+
+func _get_plugin_name() -> String:
 	return PLUGIN_NAME
 
-func _get_plugin_icon():
+func _get_plugin_icon() -> Texture2D:
 	return NovaTools.get_editor_icon_named("Animation", Vector2i.ONE * 16)
 
-func _enter_tree():
-	if EditorInterface.is_plugin_enabled(PLUGIN_NAME):
+func _enter_tree() -> void:
+	_ensure_script_docs()
+	if EditorInterface.is_plugin_enabled(PLUGIN_NAME_INTERNAL):
 		_try_init_platform()
 
-func _enable_plugin():
+func _enable_plugin() -> void:
+	_ensure_script_docs()
 	_try_init_platform()
 
-func _disable_plugin():
+func _disable_plugin() -> void:
 	_try_deinit_platform()
 
-func _exit_tree():
+func _exit_tree() -> void:
 	_try_deinit_platform()
 
-func _try_init_platform():
+func _try_init_platform() -> void:
 	if _export_platform_ref == null:
 		_export_platform_ref = VideoEditorExportPlatform.new()
 		add_export_platform(_export_platform_ref)
 
-func _try_deinit_platform():
+func _try_deinit_platform() -> void:
 	if _export_platform_ref != null:
 		remove_export_platform(_export_platform_ref)
 		_export_platform_ref = null
